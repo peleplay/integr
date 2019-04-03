db140747ce868441ecc98d6f040bea61b5862f0e
peleplay <peleplay@gmail.com>
2019-04-03 18:54:57 +0200

    Cleanup helping functions roxygen descriptions

--- R/helpingFunctions.R
+++ R/helpingFunctions.R
@@ -1,14 +1,12 @@
-#Entropy function ----
-
 #' Calculates Shannon's entropy
 #'
-#' @param df A dataframe (discrete!) (name, string)
-#' @param classAtt A class column of the df (name, string)
-#' @return the Shannon's entropy of \code{df} based on \code{classAtt}
-#' @example
-#' \dontrun{
-#' Entropy(myDataFrame, myClassAttribute)
-#' }
+#' Formula: \eqn{H(S) = -P_i * \sum log_2 * P_i}, where \eqn{P_i} is the
+#' probability of the corresponding \eqn{i}-th class
+#'
+#' @param df A (discrete) data.frame
+#' @param classAtt A class column of the df (string)
+#' @return The Shannon's entropy of \code{df}, based on the \code{classAtt}
+#'   attribute
 
 Entropy <- function(df, classAtt){
   freq <- table(df[classAtt]) #vector of class frequencies
@@ -18,58 +16,63 @@ Entropy <- function(df, classAtt){
   return(entr)
 }
 
-#Information gain (2-way interaction) function ----
-
-#' Calculates Information gain (2-way Interaction Gain) of a discrete dataframe
+#' Calculates Information Gain (2-way Interaction Gain) of a discrete dataframe
 #'
-#' @param df A dataframe (discrete!)
-#' @param inAtt An input column of the dataframe
-#' @param classAtt A class column of the dataframe
-#' @return the Information gain of \code{df} on the class attribute \code{classAtt}
-#' @example
-#' \dontrun{
-#' InfoGain(myDataFrame, myInputAttribute, myClassAttribute)
-#' }
+#' \eqn{InfoGAIN = H(S) - H(S|X)}, where \eqn{H(S)} is the difference in the
+#' Shannon's entropy of the system \eqn{S} before a new attribute \eqn{X} is
+#' introduced, and \qen{H(S|X)} is the entropy afterwards.
+#'
+#' @param df A discrete dataframe
+#' @param inAtt An input column of the dataframe (string)
+#' @param classAtt A class column of the dataframe (string)
+#' @return The Information Gain of \code{df} on the class attribute
+#'   \code{classAtt}
 
 InfoGain <- function(df, inAtt, classAtt){
   apr <- Entropy(df, classAtt) #a-priori entropy of the df
 
-  freqA <- df %>% dplyr::group_by(!!as.symbol(inAtt)) %>% dplyr::count(!!as.symbol(classAtt))
-  freqB <- freqA %>% dplyr::group_by(!!as.symbol(inAtt)) %>%
-                  dplyr::summarise(classCount = sum(n))
+  #Frequency table for the new attribute
+  freqA <- df %>%
+           dplyr::group_by(!!as.symbol(inAtt)) %>%
+           dplyr::count(!!as.symbol(classAtt))
+
+  #Frequency table for the class attribute
+  freqB <- freqA %>%
+           dplyr::group_by(!!as.symbol(inAtt)) %>%
+           dplyr::summarise(classCount = sum(n))
+
+  #Overall frequency table
   freqC <- dplyr::inner_join(freqA, freqB, by = inAtt) #'by' argument is char!
-  freqC$probs <- round(freqC$n/freqC$classCount, 3)
-  freqC$logs <- freqC$probs * log2(freqC$probs) * (-1)
+  freqC$probs <- round(freqC$n/freqC$classCount, 3) #Calculate probabilities
+  freqC$logs <- freqC$probs * log2(freqC$probs) * (-1) #Calculate logarithms
 
-  freqB$entrs <- freqC %>% dplyr::group_by(!!as.symbol(inAtt)) %>% dplyr::summarise(entrs = sum(logs))
-
+  #Upper level entropy components
+  freqB$entrs <- freqC %>%
+                 dplyr::group_by(!!as.symbol(inAtt)) %>%
+                 dplyr::summarise(entrs = sum(logs))
   freqB$weight <- round(freqB$classCount / sum(freqB$classCount),3)
 
+  #Overall entropy
   entr <- sum(freqB$weight * freqB$entrs$entrs)
 
+  #Information gain
   infgn <- apr - entr
   return(infgn)
 }
 
-#Test for discrete data frame ----
-
-#' Tests if all columns of the data.frame are discrete (i.e. factors)
+#' Tests if data.frame is discrete (i.e. all of its columns are factors)
 #'
 #' @param df A data frame
 #' @return Boolean: TRUE if all columns of the data frame \code{df}
 #' are factors, FALSE otherwise; If the provided \code{df} object is of other
 #' type than data.frame, the function returns FALSE.
-#' @example
-#' \dontrun{
-#' IsDiscreteDataFrame(myDataFrame)
-#' }
 
 IsDiscreteDataFrame <- function(df){
 
   #Check if df is data.frame
   if(is.data.frame(df)){
 
-    #Check if there is a non-discrete column
+    #Check if there is a non-discrete column; If there is, return FALSE
     if(FALSE %in% sapply(df, is.factor)){
       return(FALSE)
     }
@@ -79,7 +82,7 @@ IsDiscreteDataFrame <- function(df){
 
   }
 
-  #not a data.frame
+  #not a data.frame, return FALSE
   else{
     return(FALSE)
   }
