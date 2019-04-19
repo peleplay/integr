77a09178d4db50df6206d6d152b520c3e0bf5605
peleplay <peleplay@gmail.com>
2019-04-19 22:24:02 +0200

    Complete export (to images) functions

--- R/outputFunctions.R
+++ R/outputFunctions.R
@@ -1,4 +1,4 @@
-#Plot graphviz
+#Plot graphviz ----
 #' Plots Interaction graph
 #'
 #' @param ig Interaction graph
@@ -23,11 +23,65 @@ plotIntGraph <- function(ig) {
 
 igToGrViz <- function(ig, path = getwd(), fName = "InteractionGraph") {
 
-  #Check input validity : Depreciated
-  # if (typeof(ig) != "character" || grepl("InteractionGraph", ig) != TRUE) {
-  #   #the ig parameter does not point to InteractionGraph definition string
-  #   stop("Please provide Interaction graphs definition as the 'ig' parameter")
-  # }
+  #Check input validity
+  if (typeof(ig) != "character" || grepl("InteractionGraph", ig) != TRUE) {
+    #the ig parameter does not point to InteractionGraph definition string
+    stop("Please provide Interaction graphs definition as the 'ig' parameter")
+  }
+
+  if (typeof(ig) != "character") {
+    #the ig parameter does not point to InteractionGraph definition string
+    stop("Please provide Interaction graphs definition as the 'ig' parameter")
+  }
+
+  if (typeof(path) != "character" || grepl("\\\\", path) == TRUE) {
+    #the path parameter is not defined properly
+    stop("The 'path' parameter is not properly set. It needs to be string and
+         without \\ symbol in the path")
+  }
+
+  if (typeof(fName) != "character" || grepl(" ", fName) == TRUE) {
+    #the fName parameter is not defined properly
+    stop("The 'fName' parameter is not a proper name for the interaction graph.
+         It needs to be string and without empty spaces and extension")
+  }
+
+  #Write ig to .gv binary
+  if(path == getwd()){ #Add trailing backslash if current wd is used
+    path <- paste0(path, "/")
+  }
+
+  file <- paste0(path, fName, ".gv") #set file name
+  sink(file) #open connection
+  cat(ig) #write graphviz contents
+  sink() #close connection
+
+}
+
+#Export image (svg) ----
+
+#' Exports Interaction graph to a SVG file
+#'
+#' @param ig Interaction graph
+#' @param path The folder in which to write the SVG file; by default,
+#'   the working directory
+#' @param fName The name of the file to be created; "InteractionGraph" by
+#'   default
+#' @param h Desired height of the image in pixels; 2000px by default
+#' @return Writes the \code{ig} interaction graph to a SVG (\code{.svg}) file
+#'   to the specified folder in \code{path}
+#' @export
+
+igToSVG <- function(ig,
+                    path = getwd(),
+                    fName = "InteractionGraph",
+                    h = 2000) {
+
+  #Check input validity
+  if (typeof(ig) != "character" || grepl("InteractionGraph", ig) != TRUE) {
+    #the ig parameter does not point to InteractionGraph definition string
+    stop("Please provide Interaction graphs definition as the 'ig' parameter")
+  }
 
   if (typeof(ig) != "character") {
     #the ig parameter does not point to InteractionGraph definition string
@@ -46,71 +100,16 @@ igToGrViz <- function(ig, path = getwd(), fName = "InteractionGraph") {
          It needs to be string and without empty spaces")
   }
 
-  #Write ig to .gv binary
-  if(path == getwd()){ #Add trailing backslash if current wd is used
-    path <- paste0(path, "/")
-  }
-
-  currGraph <- paste0(path, fName, ".gv") #set file name
-  sink(currGraph) #open connection
-  cat(ig) #write graphviz contents
-  sink() #close connection
-
-}
-
-#Export image (svg) ----
-
-#' Exports Interaction graph to a SVG file
-#'
-#' @param ig Interaction graph
-#' @param path The folder in which to write the SVG file; by default,
-#'   the working directory
-#' @param fName The name of the file to be created; "InteractionGraph" by
-#'   default
-#' @param height Desired height of the image in pixels; 2000px by default
-#' @return Writes the \code{ig} interaction graph to a SVG (\code{.svg}) file
-#'   to the specified folder in \code{path}
-#' @export
-
-igToSVG <- function(ig,
-                    path = getwd(),
-                    fName = "InteractionGraph",
-                    height = 2000) {
-
-  #Check validity of input
-  #Check input validity : Depreciated
-  # if (typeof(ig) != "character" || grepl("InteractionGraph", ig) != TRUE) {
-  #   #the ig parameter does not point to InteractionGraph definition string
-  #   stop("Please provide Interaction graphs definition as the 'ig' parameter")
-  # }
-
-  if (typeof(ig) != "character") {
-    #the ig parameter does not point to InteractionGraph definition string
-    stop("Please provide Interaction graphs definition as the 'ig' parameter")
-  }
-
-  if (typeof(path) != "character" || grepl("\\", path) == TRUE) {
-    #the path parameter is not defined properly
-    stop("The 'path' parameter is not properly set. It needs to be string and
-         without \\ symbol in the path")
-  }
-
-  if (typeof(fName) != "character" || grepl(" ", fName) == TRUE) {
-    #the fName parameter is not defined properly
-    stop("The 'fName' parameter is not a proper name for the interaction graph.
-         It needs to be string and without empty spaces")
-  }
-
-  if (typeof(height) != "double" || typeof(height) != "integer") {
+  if (typeof(h) != "double") {
     #the height parameter is not defined properly
     stop("The 'height' parameter needs to be integer")
   }
 
 
   #Export ig to svg
-  ig %>% DiagrammeRsvg::export_svg(height = height,
-                                   file = paste0(path, fName, ".svg"))
-
+  DiagrammeRsvg::export_svg(grViz(ig)) %>%
+    charToRaw %>% rsvg::rsvg_svg(file=paste0(path, "/", fName, ".svg"),
+                                 height = h)
 }
 
 #Export image (png) ----
@@ -122,8 +121,7 @@ igToSVG <- function(ig,
 #'   the working directory
 #' @param fName The name of the file to be created; "InteractionGraph" by
 #'   default
-#' @param height Desired height of the image in pixels; 2000px by default
-#' @param dpi Desired resolution of the image in dpi; 300dpi by default
+#' @param h Desired height of the image in pixels; 2000px by default
 #' @return Writes the \code{ig} interaction graph to a PNG (\code{.png}) file
 #'   to the specified folder in \code{path}
 #' @export
@@ -131,22 +129,20 @@ igToSVG <- function(ig,
 igToPNG <- function(ig,
                     fName = "InteractionGraph",
                     path = getwd(),
-                    height = 2000,
-                    dpi = 300) {
+                    h = 2000) {
 
-  #Check validity of input
-  #Check input validity : Depreciated
-  # if (typeof(ig) != "character" || grepl("InteractionGraph", ig) != TRUE) {
-  #   #the ig parameter does not point to InteractionGraph definition string
-  #   stop("Please provide Interaction graphs definition as the 'ig' parameter")
-  # }
+  #Check input validity
+  if (typeof(ig) != "character" || grepl("InteractionGraph", ig) != TRUE) {
+    #the ig parameter does not point to InteractionGraph definition string
+    stop("Please provide Interaction graphs definition as the 'ig' parameter")
+  }
 
   if (typeof(ig) != "character") {
     #the ig parameter does not point to InteractionGraph definition string
     stop("Please provide Interaction graphs definition as the 'ig' parameter")
   }
 
-  if (typeof(path) != "character" || grepl("\\", path) == TRUE) {
+  if (typeof(path) != "character" || grepl("\\\\", path) == TRUE) {
     #the path parameter is not defined properly
     stop("The 'path' parameter is not properly set. It needs to be string and
          without \\ symbol in the path")
@@ -158,22 +154,16 @@ igToPNG <- function(ig,
          It needs to be string and without empty spaces")
   }
 
-  if (typeof(height) != "double" || typeof(height) != "integer") {
+  if (typeof(h) != "double") {
     #the height parameter is not defined properly
     stop("The 'height' parameter needs to be integer")
   }
 
-  if (typeof(dpi) != "double" || typeof(dpi) != "integer") {
-    #the dpi parameter is not defined properly
-    stop("The 'dpi' parameter needs to be integer")
-  }
 
   #Export ig to png
-  ig %>%
-    DiagrammeRsvg::export_svg %>%
-    charToRaw %>%
-    rsvgL::rsvg(height = height) %>%
-    png::writePNG(paste0(path, fName, ".png"), dpi = dpi)
+  DiagrammeRsvg::export_svg(grViz(ig)) %>%
+    charToRaw %>% rsvg::rsvg_png(file=paste0(path, "/", fName, ".png"),
+                                 height = h)
 
 }
 
@@ -186,7 +176,7 @@ igToPNG <- function(ig,
 #'   the working directory
 #' @param fName The name of the file to be created; "InteractionGraph" by
 #'   default
-#' @param height Desired height of the image in pixels; 2000px by default
+#' @param h Desired height of the image in pixels; 2000px by default
 #' @return Writes the \code{ig} interaction graph to a PDF (\code{.pdf}) file
 #'   to the specified folder in \code{path}
 #' @export
@@ -194,21 +184,15 @@ igToPNG <- function(ig,
 igToPDF <- function(ig,
                     fName = "InteractionGraph",
                     path = getwd(),
-                    height = 2000) {
+                    h = 2000) {
 
   #Check validity of input
-  #Check input validity : Depreciated
-  # if (typeof(ig) != "character" || grepl("InteractionGraph", ig) != TRUE) {
-  #   #the ig parameter does not point to InteractionGraph definition string
-  #   stop("Please provide Interaction graphs definition as the 'ig' parameter")
-  # }
-
-  if (typeof(ig) != "htmlwidget") {
+  if (typeof(ig) != "character" || grepl("InteractionGraph", ig) != TRUE) {
     #the ig parameter does not point to InteractionGraph definition string
     stop("Please provide Interaction graphs definition as the 'ig' parameter")
   }
 
-  if (typeof(path) != "character" || grepl("\\", path) == TRUE) {
+  if (typeof(path) != "character" || grepl("\\\\", path) == TRUE) {
     #the path parameter is not defined properly
     stop("The 'path' parameter is not properly set. It needs to be string and
          without \\ symbol in the path")
@@ -220,14 +204,15 @@ igToPDF <- function(ig,
          It needs to be string and without empty spaces")
   }
 
-  if (typeof(height) != "double" || typeof(height) != "integer") {
+  if (typeof(h) != "double") {
     #the height parameter is not defined properly
     stop("The 'height' parameter needs to be integer")
   }
 
   #Export ig to pdf
-  ig %>% DiagrammeRsvg::export_pdf(height = height,
-                                   file = paste0(path, fName, ".pdf"))
+  DiagrammeRsvg::export_svg(grViz(ig)) %>%
+    charToRaw %>% rsvg::rsvg_pdf(file=paste0(path, "/", fName, ".pdf"),
+                                 height = h)
 
 }
 
@@ -240,7 +225,7 @@ igToPDF <- function(ig,
 #'   the working directory
 #' @param fName The name of the file to be created; "InteractionGraph" by
 #'   default
-#' @param height Desired height of the image in pixels; 2000px by default
+#' @param h Desired height of the image in pixels; 2000px by default
 #' @return Writes the \code{ig} interaction graph to a PDF (\code{.ps}) file
 #'   to the specified folder in \code{path}
 #' @export
@@ -248,21 +233,15 @@ igToPDF <- function(ig,
 igToPS <- function(ig,
                     fName = "InteractionGraph",
                     path = getwd(),
-                    height = 2000) {
+                    h = 2000) {
 
   #Check validity of input
-  #Check input validity : Depreciated
-  # if (typeof(ig) != "character" || grepl("InteractionGraph", ig) != TRUE) {
-  #   #the ig parameter does not point to InteractionGraph definition string
-  #   stop("Please provide Interaction graphs definition as the 'ig' parameter")
-  # }
-
-  if (typeof(ig) != "htmlwidget") {
+  if (typeof(ig) != "character" || grepl("InteractionGraph", ig) != TRUE) {
     #the ig parameter does not point to InteractionGraph definition string
     stop("Please provide Interaction graphs definition as the 'ig' parameter")
   }
 
-  if (typeof(path) != "character" || grepl("\\", path) == TRUE) {
+  if (typeof(path) != "character" || grepl("\\\\", path) == TRUE) {
     #the path parameter is not defined properly
     stop("The 'path' parameter is not properly set. It needs to be string and
          without \\ symbol in the path")
@@ -274,13 +253,14 @@ igToPS <- function(ig,
          It needs to be string and without empty spaces")
   }
 
-  if (typeof(height) != "double" || typeof(height) != "integer") {
+  if (typeof(h) != "double") {
     #the height parameter is not defined properly
     stop("The 'height' parameter needs to be integer")
   }
 
   #Export ig to ps
-  ig %>% DiagrammeRsvg::export_ps(height = height,
-                                   file = paste0(path, fName, ".ps"))
+  DiagrammeRsvg::export_svg(grViz(ig)) %>%
+    charToRaw %>% rsvg::rsvg_ps(file=paste0(path, "/", fName, ".ps"),
+                                 height = h)
 
 }
